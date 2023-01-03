ee8809b7a7c87a4fca45eaab11b905c46a24de1c
Jinhee Han <jinheehanaaa@gmail.com>
2023-01-02 19:35:20 -0600

    007 - Working with Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'location_detail.dart';
+import 'location_list.dart';
 import 'mocks/mock_location.dart';
 import 'models/location.dart';
 
 void main() {
-  final Location mockLocation = MockLocation.FetchAny();
-  return runApp(MaterialApp(home: LocationDetail(mockLocation)));
+  final mockLocation = MockLocation.fetchAll();
+
+  return runApp(MaterialApp(home: LocationList(mockLocation)));
 }
